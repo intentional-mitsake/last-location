eba60db538884eaf8cc336ee8cb029434e105902
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2025-10-14 17:27:36 +0545

    auth page changes

--- mobile/lib/pages/auth_page.dart
+++ mobile/lib/pages/auth_page.dart
@@ -8,8 +8,54 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  bool isLogin = true;
+
+  //form key to identify the form and interact with it
+  //value of final can only be set once
+  final formKey = GlobalKey<FormState>();
+
+  //text controllers helps manage the input text
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController(); 
+
+  @override 
+  void dispose(){
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void toggle(){
+    setState(() {
+      isLogin = !isLogin;
+    });
+    emailController.clear();
+    passwordController.clear();
+    usernameController.clear();
+  }
+
+  void submit(){
+    if(formKey.currentState!.validate()){
+      final email = emailController.text;
+      final password = passwordController.text; 
+      final username = usernameController.text;
+
+      //pushes to home page after successful login or registration
+      //replaces the current page so that user cannot go back to auth page using back button
+      //navigator.of gets the navigator for the current context
+      Navigator.of(context).pushReplacementNamed('/home');
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isLogin ? 'Login' : 'Register'),
+      ),
+    );
   }
 }
